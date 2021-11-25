18bebe1f8e26ada09252383835c490ac830534cc
conrado.goncalves <53683786+conradoalexsander@users.noreply.github.com>
2021-11-25 15:38:00 +0100

    Added Activity functions

---
+++ VideoProcessor/ActivityFunctions.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace VideoProcessor
+{
+    public static class ActivityFunctions
+    {
+        [FunctionName(nameof(TranscodeVideo))]
+        public static async Task<string> TranscodeVideo([ActivityTrigger] string inputVideo, ILogger log)
+        {
+            log.LogInformation($"Transcoding {inputVideo}.");
+            //simulate doing activity
+            await Task.Delay(5000);
+
+            return $"{Path.GetFileNameWithoutExtension(inputVideo)}-transcoded.mp4";
+        }
+
+        [FunctionName(nameof(ExtractThumbNail))]
+        public static async Task<string> ExtractThumbNail([ActivityTrigger] string inputVideo, ILogger log)
+        {
+            log.LogInformation($"Extracting ThumbNail {inputVideo}.");
+            //simulate doing activity
+            await Task.Delay(5000);
+
+            return $"{Path.GetFileNameWithoutExtension(inputVideo)}-tumbnail.png";
+        }
+
+        [FunctionName(nameof(PrependIntro))]
+        public static async Task<string> PrependIntro([ActivityTrigger] string inputVideo, ILogger log)
+        {
+            var introLocation = Environment.GetEnvironmentVariable("IntroLocation");
+
+            log.LogInformation($"Prepending Intro {introLocation} to {inputVideo}.");
+
+            //simulate doing activity
+            await Task.Delay(5000);
+
+            return $"{Path.GetFileNameWithoutExtension(inputVideo)}-withintro.mp4";
+        }
+    }
+}
